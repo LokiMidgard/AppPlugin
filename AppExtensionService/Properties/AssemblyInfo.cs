c9443e59870bb48e76de0ff38447690d37d52d7c
LokiMidgard <patrick-kranz@live.de>
2016-11-14 12:18:30 +0100

    Added Author Name in Assembly

--- AppExtensionService/Properties/AssemblyInfo.cs
+++ AppExtensionService/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AppExtensionService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Patrick Kranz")]
 [assembly: AssemblyProduct("AppExtensionService")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
